061ca239502a158ca0c4fa63982e8563a9993032
danielFernandezJd <mdanif9728@gmail.com>
2024-04-04 09:40:56 -0700

    saving & finish

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,17 @@
 
 // Your Code Here
+async function changeBookName() {
+    let response = await fetch('http://localhost:3001/updateBook ', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'id': 3,
+            'title': "The Annals of Arathrae",
+        }),
+    })
+    let result = await response.json()
+    console.log(result)
+}
+
