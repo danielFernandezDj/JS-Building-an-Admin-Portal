32aa3006cbb4b057b8dbf6792266ff64d2524779
danielFernandezJd <mdanif9728@gmail.com>
2024-04-07 18:04:25 -0700

    regular saving

--- public/admin.js
+++ public/admin.js
@@ -1,24 +1,4 @@
-/*
-3. Deliverables
-Using the API documentation as a guide for making fetch requests 
-and what you've learned about the DOM throughout this course, write 
-code in public/admin.js to fulfill the following deliverables:
-
-Retrieve a list of books from the server.
-Display a list of book titles to the admin.
-Place a text input next to each book title.
-Give each text input a value: the quantity of the associated book.
-Place a submit button next to each text input.
-When the submit button is clicked, retrieve the quantity from the 
-associated text input and save the updated quantity to the server.
-The page already includes the CSS framework Bootstrap if you would 
-like to use its classes to style your DOM elements. Just remember 
-this is an admin page that only your friend will see, so try to 
-prioritize functionality, including the bonus deliverables, over 
-styling. When you're finished, the page should work something 
-like this:
-*/ 
-//! Your Code Here.
+//! Testing the Server.
 // async function changeBookName() {
 //     let response = await fetch('http://localhost:3001/updateBook ', {
 //         method: 'PATCH',
@@ -36,9 +16,40 @@ like this:
 
 // changeBookName()
 
+//! 3. Deliverables
+/*
+Using the API documentation as a guide for making fetch requests 
+and what you've learned about the DOM throughout this course, write 
+code in public/admin.js to fulfill the following deliverables:
+ 
+Retrieve a list of books from the server.
+Display a list of book titles to the admin.
+Place a text input next to each book title.
+Give each text input a value: the quantity of the associated book.
+Place a submit button next to each text input.
+When the submit button is clicked, retrieve the quantity from the 
+associated text input and save the updated quantity to the server.
+The page already includes the CSS framework Bootstrap if you would 
+like to use its classes to style your DOM elements. Just remember 
+this is an admin page that only your friend will see, so try to 
+prioritize functionality, including the bonus deliverables, over 
+styling. When you're finished, the page should work something 
+like this:
+*/
+
 //! The solution 3.
 let rootDiv = document.getElementById('root')
 
-function newParagraoh() {
-    rootDiv.
+function newParagraph() {
+    // create our paragraph. 
+    let createNew = document.createElement('p')
+
+    // give a name to our paragraph.
+    createNew.textContent = 'hello'
+
+    // append the Child to the div = 'root'.
+    rootDiv.appendChild(createNew);
+
+    // Log the created paragraph to the console
+    console.log(`${createNew} is working`)
 }
