d3182478eb38f7879c1f139789bb338952a6718a
danielFernandezJd <mdanif9728@gmail.com>
2024-04-07 21:24:38 -0700

    regular saving

--- public/admin.js
+++ public/admin.js
@@ -12,28 +12,26 @@
 //     let result = await response.json()
 //     console.log(result)
 // }
-
 // changeBookName()
 
-// ! 3. Deliverables
-/*
-Using the API documentation as a guide for making fetch requests 
-and what you've learned about the DOM throughout this course, write 
-code in public/admin.js to fulfill the following deliverables:
+/* //! Part 3. Deliverables
+// Using the API documentation as a guide for making fetch requests 
+// and what you've learned about the DOM throughout this course, write 
+// code in public/admin.js to fulfill the following deliverables:
  
-Retrieve a list of books from the server.
-Display a list of book titles to the admin.
-Place a text input next to each book title.
-Give each text input a value: the quantity of the associated book.
-Place a submit button next to each text input.
-When the submit button is clicked, retrieve the quantity from the 
-associated text input and save the updated quantity to the server.
-The page already includes the CSS framework Bootstrap if you would 
-like to use its classes to style your DOM elements. Just remember 
-this is an admin page that only your friend will see, so try to 
-prioritize functionality, including the bonus deliverables, over 
-styling. When you're finished, the page should work something 
-like this:
+// Retrieve a list of books from the server.
+// Display a list of book titles to the admin.
+// Place a text input next to each book title.
+// Give each text input a value: the quantity of the associated book.
+// Place a submit button next to each text input.
+// When the submit button is clicked, retrieve the quantity from the 
+// associated text input and save the updated quantity to the server.
+// The page already includes the CSS framework Bootstrap if you would 
+// like to use its classes to style your DOM elements. Just remember 
+// this is an admin page that only your friend will see, so try to 
+// prioritize functionality, including the bonus deliverables, over 
+// styling. When you're finished, the page should work something 
+// like this:
 */
 
 //? Test: display a paragraph.
@@ -69,8 +67,7 @@ async function main() {
     console.log(books)
 }
 
-// ! First Input.
-
+// ! Input solution.
 function bookInputs(book) {
     let rootDiv = document.getElementById('root')
         rootDiv.style.color = 'blue';
@@ -82,29 +79,30 @@ function bookInputs(book) {
     let li = document.createElement('li')
     li.textContent = book.title
 
-    // create new 'input'. 
-    let quantityInputs = document.createElement('input')
-    quantityInputs.value = book.quantity
+    // create new 'inputs'. 
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
 
     // create a save 'button'.
     let saveButton = document.createElement('button')
     saveButton.textContent = 'Save'
 
     saveButton.addEventListener('click', () => {
-        fetch('http://localhost:3001/listBooks', {
+        fetch('http://localhost:3001/updateBook', {
             method: 'PATCH',
             headers: {
-                'Content-Type': 'application.json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 id: book.id,
-                quantity: quantityInputs.value
+                quantity: quantityInput.value
             })
         })
     })
     
-    // append the new Child's.
-    rootDiv.appendChild(li);
+    // append section.
+    li.append(quantityInput, saveButton)
+    rootDiv.append(li);
 
     console.log(`${rootDiv} is working`)
 }
